357fa44135eb25afc9fc9e87f80d52522ee84d44
AlirezaTkQliro <alireza.tavakol@qliro.com>
2022-06-07 10:42:42 +0200

    Auth roles guarding

--- src/api/auth/roles/role.guard.ts
+++ src/api/auth/roles/role.guard.ts
@@ -15,7 +15,6 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
-    console.log({ user, requiredRoles });
 
     return user.roles?.includes(requiredRoles);
   }
